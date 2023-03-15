3e4dd8c02bd36f50e441456591ad1307f89e86af
Ned Loynd <ned@loynd.com>
2023-03-15 12:38:07 +1100

    Add version check test

---
+++ test/versionCheck.c
@@ -0,0 +1,12 @@
+#include <macrOS/FindVersion.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv) {
+    macrOS_IFR_RUNTIME_LESS_MAC_OS_MM(10, 0) {
+        return EXIT_FAILURE;
+    }
+    macrOS_IFR_RUNTIME_HAS_MAC_OS_MM(999, 0) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
